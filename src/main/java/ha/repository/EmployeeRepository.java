e1681e908fc01192d88f8b72fa27a31cc21651e4
halarotu <hannes.alarotu@gmail.com>
2016-12-11 17:29:39 +0200

    added EmployeeRepository

---
+++ src/main/java/ha/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package ha.repository;
+
+import ha.domain.Employee;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+	
+
+}
