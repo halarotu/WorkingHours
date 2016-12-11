d9fdbc04231bc5dccdc2de6304e617b946984f6c
halarotu <hannes.alarotu@gmail.com>
2016-12-11 18:30:23 +0200

    added EmployeeController and employees.html + ControllerTest

---
+++ src/main/java/ha/controller/EmployeeController.java
@@ -0,0 +1,38 @@
+package ha.controller;
+
+import ha.domain.Employee;
+import ha.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+	
+	@Autowired
+	private EmployeeRepository emplRepo;
+	
+	@RequestMapping(method = RequestMethod.GET)
+    public String getEmployees(Model model) {
+        
+		model.addAttribute("employees", this.emplRepo.findAll());
+		
+		return "employees";
+    }
+	
+	@RequestMapping(method = RequestMethod.POST)
+    public String addNewEmployee(@RequestParam String name, @RequestParam String level) {
+        Employee e = new Employee();
+        e.setName(name);
+        e.setCompanyLevel(Integer.parseInt(level));
+        
+        this.emplRepo.save(e);
+		
+		return "redirect:/employees";
+    }
+	
+}
---
+++ src/test/java/ha/ControllerTest.java
@@ -0,0 +1,65 @@
+package ha;
+
+import ha.domain.Employee;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ControllerTest {
+	
+	@Autowired
+    private WebApplicationContext context;
+	
+    private MockMvc mockMvc;
+	
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+    
+	@Test
+	public void employeeControllerTest() throws Exception {
+		
+		// basic get employees page ok? 
+		mockMvc.perform(get("/employees"))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(model().attributeExists("employees"))
+                .andExpect(view().name("employees"))
+                .andReturn();
+		
+		// adding new employee ok?
+        String name = "Teuvo Tyolainen";
+        String companyLevel = "1";
+        mockMvc.perform(post("/employees").param("name", name).param("level", companyLevel))
+                .andExpect(status().is3xxRedirection())
+                .andReturn();
+        
+        // new employee added?
+        MvcResult result = mockMvc.perform(get("/employees"))
+                .andExpect(status().is2xxSuccessful())
+                .andReturn();
+        List<Employee> employees = new ArrayList<>((Collection<Employee>) result.getModelAndView().getModel().get("employees"));
+        assertTrue("New employee was added.", employees.size() == 1);
+	}
+
+}
