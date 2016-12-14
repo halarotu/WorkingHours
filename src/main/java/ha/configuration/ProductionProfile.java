08b4e46a1887515621e6e78beb155e2442e48d2e
halarotu <hannes.alarotu@gmail.com>
2016-12-14 21:08:27 +0200

    changed: trying new configuration to fix heroku problems

--- src/main/java/ha/configuration/ProductionProfile.java
+++ src/main/java/ha/configuration/ProductionProfile.java
@@ -3,10 +3,14 @@ package ha.configuration;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import javax.sql.DataSource;
+
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
 
@@ -16,8 +20,7 @@ import org.springframework.context.annotation.Profile;
 @Profile("production")
 public class ProductionProfile {
 	
-	@Bean
-	@ConfigurationProperties(prefix = "spring.datasource")
+	/*@Bean
     public BasicDataSource dataSource() throws URISyntaxException {
         URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
@@ -31,6 +34,13 @@ public class ProductionProfile {
         basicDataSource.setPassword(password);
 
         return basicDataSource;
+    }*/
+	
+	@Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
     }
 	
 }
